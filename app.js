4a5ac1e7f69b2900baf1dd8d50f5bc6e654b7a0e
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-11-06 15:35:17 -0500

    post req for login level 1 security

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
   const newUser = new User({
-    email: req.body.username
+    email: req.body.username,
     password: req.body.password
   });
 
@@ -46,7 +46,22 @@ app.post("/register", function(req, res){
   });
 });
 
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
 
+  User.findOne({email: username}, function(err, foundUser){
+    if (err){
+      console.log(err);
+    }else{
+      if (foundUser) {
+        if (foundUser.password=== password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
 
 
 
