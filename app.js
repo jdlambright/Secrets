8696ea80aaff52da5cd6c431ad43f1f7b404f325
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-11-06 16:13:32 -0500

    started basic encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encrption");
 
 const app = express();
 
@@ -12,10 +13,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+const secret = "thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret:secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
